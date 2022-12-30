b1811831c0c0a3b5250504663635911b4bafb18b
Josh Duff <me@JoshDuff.com>
2022-12-29 18:29:52 -0600

    Constrain left/right arrow cursor navigation

--- shared/fully_managed_terminal.js
+++ shared/fully_managed_terminal.js
@@ -15,9 +15,12 @@ const backspace_value = 127
 
 const up_arrow = Buffer.from([ 27, 91, 65 ])
 const down_arrow = Buffer.from([ 27, 91, 66 ])
+const right_arrow = Buffer.from([ 27, 91, 67 ])
+const left_arrow = Buffer.from([ 27, 91, 68 ])
 
 export default ({ prompt_callback, line_prompt = `> `, input = process.stdin, output = process.stdout }) => {
 	let lines_written = 0
+	let probable_cursor_position = 0
 	let current_line_so_far = ``
 
 	const write_newline = () => {
@@ -58,8 +61,19 @@ export default ({ prompt_callback, line_prompt = `> `, input = process.stdin, ou
 			const char_code = character.charCodeAt(0)
 
 			// output.write(`buffer values: "${Array.from(chunk.values()).join(`, `)}"\n`)
-			if (char_code === backspace_value || down_arrow.equals(chunk) || up_arrow.equals(chunk)) {
-				if (current_line_so_far.length > 0) {
+			const up_or_down_arrow_key = down_arrow.equals(chunk) || up_arrow.equals(chunk)
+
+			const trying_to_move_too_far_left = left_arrow.equals(chunk) && probable_cursor_position <= 0
+			const trying_to_move_too_far_right = right_arrow.equals(chunk) && probable_cursor_position >= current_line_so_far.length
+
+			if (up_or_down_arrow_key || trying_to_move_too_far_left || trying_to_move_too_far_right) {
+				// Do nothing
+			} else if (char_code === backspace_value) {
+				// Swallow backspace keypresses by default
+				const may_backspace = current_line_so_far.length > 0
+
+				if (may_backspace) {
+					probable_cursor_position -= 1
 					current_line_so_far = current_line_so_far.slice(0, -1)
 					output.write(ansi.cursor.move(-1, 0))
 					output.write(` \b`)
@@ -67,13 +81,22 @@ export default ({ prompt_callback, line_prompt = `> `, input = process.stdin, ou
 			} else if (character === ctrl_c) {
 				process.exit()
 			} else if (char_code === 13 || character === `\n`) {
+				probable_cursor_position = 0
 				const line = current_line_so_far
 				current_line_so_far = ``
 				const update = log(line)
 				prompt_callback(line, update)
 			} else {
 				output.write(character)
-				current_line_so_far += character
+
+				if (left_arrow.equals(chunk)) {
+					probable_cursor_position -= 1
+				} else if (right_arrow.equals(chunk)) {
+					probable_cursor_position += 1
+				} else {
+					current_line_so_far += character
+					probable_cursor_position += character.length
+				}
 			}
 
 			callback()
